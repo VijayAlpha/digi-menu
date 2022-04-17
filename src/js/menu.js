8d047e7a52a663642aed318a9a9c72c067713c1f
Vijay M <vijaymurugan177@gmail.com>
2022-04-17 19:11:05 +0530

    commit

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,7 +3,7 @@ const dishMarkUp = (dish) => {
     <div class="recipe__details flex__btw" id="${dish._id}">
     <div class="flex flex__btw" style="flex: 1">
       <div class="recipe__info">
-        <span class="recipe__info-data recipe__info-data--people"
+        <span class="recipe__info-data recipe__info-data--people" id="name-${dish._id}"
           >${dish.DishName}</span
         >
         <span class="recipe__info-text" style="margin-left: 4rem">
@@ -21,13 +21,6 @@ const dishMarkUp = (dish) => {
         <span class="recipe__info-text">min</span>
       </div>
       <div class="recipe__info">
-        <svg class="recipe__info-icon">
-          <use href="src/img/icons.svg#icon-users"></use>
-        </svg>
-        <span class="recipe__info-data recipe__info-data--people"
-          >0</span
-        >
-        <span class="recipe__info-text"></span>
       </div>
     </div>
     <div class="flex">
@@ -36,7 +29,7 @@ const dishMarkUp = (dish) => {
           <use href="src/img/icons.svg#icon-minus-circle"></use>
         </svg>
       </div>
-      <button class="btn--round">
+      <button class="btn--round plus-btn">
         <svg>
           <use href="src/img/icons.svg#icon-plus-circle"></use>
         </svg>
@@ -46,29 +39,118 @@ const dishMarkUp = (dish) => {
     `
 }
 
-const dishList = (data) => {
+const orderListMarkup = (data)=>{
     return `
-        <div class="flex flex__btw">
-            <p class="preview__publisher">${data.dishName}</p>
-            <p class="preview__publisher">${data.Servings}</p>
-        </div>`
+    <li class="recipe__ingredient">
+    <svg class="recipe__icon">
+      <use href="src/img/icons.svg#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${data[1]}</div>
+    <div class="recipe__description">${data[0]}</div>
+  </li>`
 }
 
 const elementResult = document.querySelector('.dish-box');
+const elementAddOrder = document.querySelector('.orderListHere');
+const elementOrder = document.querySelector('#place-order-btn');
+const state = {
+    order: []
+};
 
+let finalOrder = [];
+let placeOrder = []
 const renderResult = (data)=>{
     data.forEach((item)=>{
         elementResult.insertAdjacentHTML('afterbegin' , dishMarkUp(item));
     })
 }
 
+const renderOrder = (data )=>{
+    data.forEach((item)=>{
+        elementAddOrder.insertAdjacentHTML('beforeend' , orderListMarkup(item));
+    })
+}
 
-const init = ()=>{
+const convertOrder = (order)=> {
+  console.log(order);
 
-    fetch('http://127.0.0.1:3000/getDish')
+  order.forEach((el , i)=>{
+    placeOrder.push({
+      dishName: el[0],
+      Servings: el[1]
+    })
+  })
+}
+
+elementResult.addEventListener('click' , (e)=>{
+    if( e.target.parentElement.className == 'btn--round plus-btn'){
+        const dishId = e.target.parentElement.parentElement.parentElement.id;
+        const counts = {};
+        state.order.push(document.getElementById(`name-${dishId}`).innerText);
+        elementAddOrder.innerHTML = ""
+        
+
+        state.order.forEach(function (x) { 
+            counts[x] = (counts[x] || 0) + 1; 
+        });
+
+       // console.log(Object.getOwnPropertyNames(counts))
+       finalOrder = Object.entries(counts);
+       renderOrder(finalOrder);
+       console.log(finalOrder);
+    }
+
+    if( e.target.parentElement.className == 'recipe__user-generated'){
+        const dishId = e.target.parentElement.parentElement.parentElement.id;
+        const index = state.order.indexOf(document.getElementById(`name-${dishId}`).innerText);
+        const counts = {};
+
+        if (index > -1) {
+            state.order.splice(index, 1); // 2nd parameter means remove one item only
+        }
+          console.log(state.order);
+          elementAddOrder.innerHTML = ""
+         
+        state.order.forEach(function (x) { 
+          counts[x] = (counts[x] || 0) + 1; 
+          });
+
+        // console.log(Object.getOwnPropertyNames(counts))
+        finalOrder = Object.entries(counts);
+        renderOrder(finalOrder);
+        console.log(finalOrder);
+    }
+})  
+
+elementOrder.addEventListener('click', async(e)=> {
+  e.preventDefault();
+
+  try {
+    convertOrder(finalOrder);
+    console.log(placeOrder)
+
+    const data = {
+      Name : document.getElementById('costumerName').value,
+      orders: placeOrder,
+    }
+    const res = await axios({
+        method: 'POST',
+        url: 'http://127.0.0.1:3000/order',
+        data,
+    });
+
+    location.assign('/cart.html');
+} catch (err) {
+    console.log(err);
+}
+})
+
+
+const init = async ()=>{
+
+    await fetch('http://127.0.0.1:3000/getDish')
     .then(response => response.json())
     .then(data => {
-      console.log(data.dish);
         renderResult(data.dish)
     });
 }
